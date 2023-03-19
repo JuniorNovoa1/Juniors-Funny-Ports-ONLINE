d1fb6692654617ba504b4045809d7b0c514476dc
Junior <87715171+JuniorNovoa1@users.noreply.github.com>
2023-03-18 22:22:48 -0400

    fa

---
+++ mods/data/apple-leak/script.lua
@@ -0,0 +1,404 @@
+local canChangeIcon = true;
+local alphaChange = false;
+local bfX = 0;
+local bfY = 0;
+local dadX = 0;
+local dadY = 0;
+local zoomStuff = 0.05;
+
+local stayOnCam = false;
+local camX = 0;
+local camY = 0;
+
+local loopVarThing = false;
+
+function onCreatePost()
+	local randomNumb = 0;
+	randomNumb = getRandomInt(1, 80);
+
+	addHaxeLibrary('FlxObject', 'flixel')
+	addHaxeLibrary('FlxBasic', 'flixel')
+	addHaxeLibrary('HealthIcon')
+	runHaxeCode([[
+		var rando = ]]..randomNumb..[[;
+		var icon = new HealthIcon('fatbandu/' + rando, false);
+		add(icon);
+	]]) --doesn't work but it was worth a shot.
+	--setProperty('cpuControlled', true) --testing loll
+end
+
+function onUpdate()
+	if getProperty('iconP2.visible') == true and canChangeIcon then
+		setProperty('iconP2.visible', false)
+	end
+
+	if getProperty('iconP2.visible') == false and canChangeIcon then
+		runHaxeCode([[
+			icon.x = game.iconP2.x;
+			icon.y = game.iconP2.y;
+			icon.width = game.iconP2.width;
+			icon.height = game.iconP2.height;
+		]])
+	end
+	if getProperty('iconP2.visible') == false and not canChangeIcon then
+		setProperty('iconP2.visible', true)
+		runHaxeCode([[
+			destroy(icon);
+		]])
+	end
+
+	time = 1.5;
+
+	if alphaChange and getProperty('dad.alpha') == 1 then
+		doTweenAlpha('lmaoDad', 'dad', 0, time)
+	end
+
+	if alphaChange and getProperty('dad.alpha') == 0 then
+		doTweenAlpha('lmaoDad', 'dad', 1, time)
+	end
+
+	if stayOnCam then
+		setProperty('camFollow.x', camX);
+		setProperty('camFollow.y', camY);
+	end
+end
+
+function onStepHit()
+	if curStep == 1088 then
+		doTweenY('banduFat', 'dad', getProperty('dad.y') -500, 40)
+	end
+
+	if curStep == 1855 then
+		--unused
+		dadX = getProperty('dad.x');
+		dadY = getProperty('dad.y');
+	end
+
+	--[[local zoomStuff = 0.05;
+
+	if curStep == 3200 then
+		setProperty('defaultCamZoom', getProperty('defaultCamZoom') -zoomStuff)
+		removeLuaSprite('garryLeak', true)
+		setProperty('r2.visible', true)
+		alphaChange = true;
+	end--]]
+
+	if curStep == 5087 then --5088
+		--[[
+		setProperty('defaultCamZoom', getProperty('defaultCamZoom') +zoomStuff)
+		setProperty('th1ft.visible', true)
+		removeLuaSprite('r1', true)
+		removeLuaSprite('r2', true)
+		alphaChange = false;
+		setProperty('dad.alpha', 1)
+		--]]
+		--setProperty('dad.y' -9999) --3d leak bambi would keep showing
+	end
+
+	if curStep == 5744 then
+		--youtoob
+	end
+
+	if curStep == 6848 then
+		--funk
+	end
+
+	if curStep == 7488 then
+		--portal
+		setProperty('portal.visible', true)
+		setProperty('portal.x', getProperty('boyfriend.x') + 205)
+		setProperty('portal.y', getProperty('boyfriend.y') + 205)
+		setProperty('portal.scale.x', 1)
+		setProperty('portal.scale.y', 1)
+		doTweenX('null1', 'portal.scale', 0, 1)
+		doTweenY('invisPortal', 'portal.scale', 0, 1)
+		doTweenX('null3', 'boyfriend.scale', 0, 1)
+		doTweenY('null4', 'boyfriend.scale', 0, 1)
+	end
+
+	if curStep == 9696 then
+		--static for prob 2 secs
+	end
+
+	if curStep == 10464 then
+		--static for prob 2 secs
+	end
+
+	if curStep == 11424 then
+		--static for prob 2 secs
+	end
+
+	if curStep == 11944 then
+		--discord leave call
+	end
+end
+
+function onEvent(name, value1, value2)
+	if name == 'Change Character' then
+		if value2 == 'fat-bandu-3d' then
+			changeNoteSkin(false, 'NOTE_assets_3D'); --adding what im guessing is gonna be in the newest build since leaked build is 4 months old.
+
+			setProperty('wavyApplecore.visible', true)
+			canChangeIcon = false;
+		end
+
+		if value2 == 'gary' then
+			setProperty('fatBandu.x', dadX)
+			setProperty('fatBandu.y', dadY)
+			setProperty('fatBandu.visible', true)
+	
+			doTweenX('gasStationWidth', 'gasStation.scale', 0, 1)
+			doTweenY('gasStationHeigth', 'gasStation.scale', 0, 1)
+			doTweenAlpha('gasStationAlpha', 'gasStation', 0, 1)
+	
+			doTweenX('wavyWidth', 'wavyApplecore.scale', 0, 1)
+			doTweenY('wavyHeigth', 'wavyApplecore.scale', 0, 1)
+			doTweenAlpha('wavyAlpha', 'wavyApplecore', 0, 1)
+	
+			doTweenAlpha('fatBandunAlpha', 'fatBandu', 0, 1)
+			doTweenAlpha('jamboulAlpha', 'jamboul', 0, 1)
+	
+			doTweenY('kineMasterUp', 'kineMaster', getProperty('kineMaster.y') -125, 1.25)
+			doTweenAngle('kineMasterAngle', 'kineMaster', 90, 1.5)
+		end
+
+		if value2 == '3d-bambi-leak' then
+			setProperty('defaultCamZoom', getProperty('defaultCamZoom') -zoomStuff)
+			removeLuaSprite('garryLeak', true)
+			setProperty('r2.visible', true)
+			alphaChange = true;
+		end
+
+		if value2 == 'th1ft' then
+			changeNoteSkin(false, 'NOTE_assets'); --adding what im guessing is gonna be in the newest build since leaked build is 4 months old.
+
+			zoomStuff = zoomStuff + 0.15;
+			setProperty('defaultCamZoom', getProperty('defaultCamZoom') +zoomStuff)
+			setProperty('th1ft.visible', true)
+			removeLuaSprite('r1', true)
+			removeLuaSprite('r2', true)
+			alphaChange = false;
+			setProperty('dad.alpha', 1)
+
+			bfX = getProperty('boyfriend.x');
+			bfY = getProperty('boyfriend.y');
+		end
+
+		if value2 == 'bandu-trolled' then
+			changeNoteSkin(false, 'NOTE_assets_3D'); --adding what im guessing is gonna be in the newest build since leaked build is 4 months old.
+
+			setProperty('ytVids.scale.x', 2)
+			setProperty('ytVids.scale.y', 2)
+			doTweenX('ytVidsWidth', 'ytVids.scale', 1, 1)
+			doTweenY('ytVidsHeigth', 'ytVids.scale', 1, 1)
+			camX = getProperty('camFollow.x');
+			camY = getProperty('camFollow.y');
+			stayOnCam = true;
+
+			setProperty('dad.x', 25)
+			setProperty('dad.y', 1290)
+			
+			setProperty('boyfriend.x', 1400)
+			setProperty('boyfriend.y', 1625)
+			makeLuaSprite('boyfond', 'appleLeak/trolled/boyfranon', bfX, bfY)
+			addLuaSprite('boyfond', false)
+
+			setProperty('ytStatic.visible', true)
+			setProperty('ytBox.visible', true)
+		end
+
+		if value2 == 'sammy' then
+			setProperty('funkipedia.visible', true)
+			setProperty('dad.x', 1660)
+			setProperty('dad.y', 2070)
+			setProperty('boyfriend.x', 1250)
+			setProperty('boyfriend.y', 2500)
+			setProperty('boyfriend.scale.x', 0.85)
+			setProperty('boyfriend.scale.y', 0.85)
+		end
+
+		if value2 == 'duelExpunged' then
+			setProperty('defaultCamZoom', 0.8)
+			removeLuaSprite('funkipedia', true)
+			removeLuaSprite('ytStatic', true)
+			removeLuaSprite('ytBox', true)
+			setProperty('caveBG.visible', true)
+			setProperty('brob.visible', true)
+			setProperty('monkey.visible', true)
+			setProperty('ohyeah.visible', true)
+			setProperty('wrath.visible', true)
+			setProperty('caveFloor.visible', true)
+			setProperty('norman.visible', true)
+
+			setProperty('dad.x', -300)
+			setProperty('dad.y', 350)
+			setProperty('boyfriend.x', 900)
+			setProperty('boyfriend.y', 465) --765
+
+			setProperty('portal.visible', true)
+			setProperty('portal.x', getProperty('boyfriend.x') + 200)
+			setProperty('portal.y', getProperty('boyfriend.y') + 200)
+			setProperty('boyfriend.scale.x', 0)
+			setProperty('boyfriend.scale.y', 0)
+
+			doTweenX('portalWidth', 'portal.scale', 1, 1)
+			doTweenY('portalHeight', 'portal.scale', 1, 1)
+			doTweenX('portalBFWidth', 'boyfriend.scale', 1, 1)
+			doTweenY('portalBFHeight', 'boyfriend.scale', 1, 1)
+		end
+
+		if value2 == '3d-bambi-leak-finale' then
+			if loopVarThing == false then
+				removeLuaSprite('caveBG', true)
+				removeLuaSprite('brob', true)
+				removeLuaSprite('monkey', true)
+				removeLuaSprite('ohyeah', true)
+				removeLuaSprite('wrath', true)
+				removeLuaSprite('caveFloor', true)
+				removeLuaSprite('norman', true)
+				setProperty('fire.visible', true)
+				setProperty('vg.visible', true)
+				setProperty('dad.x', 250)
+				setProperty('dad.y', -315)
+				setProperty('dad.visible', false)
+				setProperty('camHUD.visible', false)
+				camX = 1290;
+				camY = 450;
+				stayOnCam = true;
+
+				setProperty('boyfriend.y', getProperty('boyfriend.y') - 900)
+				doTweenY('boyfrinedYYY', 'boyfriend', getProperty('boyfriend.y') + 900, 1.5)
+				loopVarThing = true;
+			end
+
+			changeNoteSkin(true, 'NOTE_assets_3D'); --adding what im guessing is gonna be in the newest build since leaked build is 4 months old.
+		else
+			setProperty('dad.x', 250)
+			setProperty('dad.y', -315)
+		end
+
+		if value2 == 'duel-cheating-bambi-leak' then
+			--1 second static
+		end
+
+		if value2 == 'wide-bambi-leak' then
+			--2 second static
+		end
+
+		if value2 == 'screwed-bambi-leak' then
+			--2 second static
+		end
+		
+		if value2 == 'among-bambi' then
+			--2 second static
+		end
+
+		if value2 == 'bob-bambi-leak' then
+			--2 second static
+		end
+	end
+end
+
+function onTweenCompleted(tag)
+	if tag == 'jamboulAlpha' then
+		removeLuaSprite('jamboul', true)
+		removeLuaSprite('fatBandu', true)
+		removeLuaSprite('gasStation', true)
+		removeLuaSprite('wavyApplecore', true)
+		doTweenX('garyMoveX', 'dad', dadX, 1.5)
+		doTweenY('garyMoveY', 'dad', dadY, 1.5)
+	end
+
+	if tag == 'kineMasterAngle' then
+		doTweenY('kineMasterDown', 'kineMaster', 1000, 2.6)
+		doTweenAngle('kineMasterAngle2', 'kineMaster', 125, 2.6)
+	end
+
+	if tag == 'kineMasterAngle2' then
+		removeLuaSprite('kineMaster', true)
+	end
+
+	if tag == 'ytVidsHeigth' then
+		doTweenAngle('boyfondAngle', 'boyfond', 10, 0.2)
+	end
+
+	if tag == 'boyfondAngle' then
+		doTweenAngle('boyfondAngle2', 'boyfond', 120, 1)
+		doTweenAlpha('boyfondAlpha', 'boyfond', 0, 1)
+	end
+
+	if tag == 'boyfondAlpha' then
+		removeLuaSprite('boyfond', true)
+		stayOnCam = false;
+	end
+
+	if tag == 'invisPortal' then
+		setProperty('portal.visible', false)
+	end
+
+	if tag == 'portalBFHeight' then
+		doTweenY('null5', 'boyfriend', 765, 1)
+		doTweenX('null1', 'portal.scale', 0, 1)
+		doTweenY('invisPortal', 'portal.scale', 0, 1)
+	end
+
+	if tag == 'camHUDHeight' then
+		doTweenX('null999', 'camHUD.scale', 1, 1.25, 'cubeInOut')
+		doTweenY('null314', 'camHUD.scale', 1, 1.25, 'cubeInOut')
+	end
+end
+
+local POP = false;
+local POP2 = false;
+local POP3 = false;
+
+function opponentNoteHit(id, direction, noteType, isSustainNote)
+	if getProperty('dad.curCharacter') == 'bandu-trolled' and not POP then
+		doTweenX('dadWidth', 'dad.scale', 0.3, 0.35)
+		doTweenY('dadHeigth', 'dad.scale', 0.3, 0.35)
+		removeLuaSprite('ytVids', true)
+		removeLuaSprite('th1ft', true)
+		POP = true;
+	end
+
+	if getProperty('dad.curCharacter') == '3d-bambi-leak-finale' and not POP2 then
+		setProperty('camHUD.scale.x', 1.5)
+		setProperty('camHUD.scale.y', 1.5)
+		setProperty('camHUD.visible', true)
+		doTweenX('camHUDWidth', 'camHUD.scale', 0.5, 1.25, 'cubeInOut')
+		doTweenY('camHUDHeight', 'camHUD.scale', 0.5, 1.25, 'cubeInOut')
+		triggerEvent("camFlash", '0.5', '')
+		setProperty('dad.visible', true)
+		stayOnCam = false;
+		POP2 = true;
+	end
+
+	if getProperty('dad.curCharacter') == 'among-bambi' and not POP3 then
+		--flash twitter post
+	end
+end
+
+function changeNoteSkin(player, skin)
+	if player == true then
+		for i = 0, 4, 1 do
+			setPropertyFromGroup('playerStrums', i, 'texture', skin)
+		end
+	end
+    if not player then
+		for i = 0, 4, 1 do
+			setPropertyFromGroup('opponentStrums', i, 'texture', skin)
+		end
+	end
+
+    for i = 0, getProperty('notes.length') -1 do
+        if getPropertyFromGroup('notes', i, 'mustPress') == player then --only "player" side
+            setPropertyFromGroup('notes', i, 'texture', skin)
+        end
+    end
+
+    for i = 0, getProperty('unspawnNotes.length') -1 do
+        if getPropertyFromGroup('unspawnNotes', i, 'mustPress') == player then --only "player" side
+            setPropertyFromGroup('unspawnNotes', i, 'texture', skin)
+        end
+    end
+end
---
+++ mods/data/ferocious/script.lua
@@ -0,0 +1,406 @@
+local normalStrumY = 50;
+local daCurstep = 0;
+local padZoomTime = 1;
+local allowCountdown = false;
+local allowSongEnd = false;
+
+--[[
+function onStartCountdown()
+	if allowCountdown then
+		return Function_Continue;
+	end
+
+	if not allowCountdown then
+		makeLuaSprite('greenScreen', '', 0, 0)
+		makeGraphic('greenScreen', '1280', '720', '90ee90')
+		setObjectCamera('greenScreen', 'camother')
+		scaleObject('greenScreen', 10, 10)
+		screenCenter('greenScreen', 'x')
+		screenCenter('greenScreen', 'y')
+		updateHitbox('greenScreen')
+		addLuaSprite('greenScreen', false)
+
+		makeLuaSprite('garrett1', 'funnyAnimal/fat_guy', 0, 0)
+		setObjectCamera('garrett1', 'camother')
+		screenCenter('garrett1', 'x')
+		screenCenter('garrett1', 'y')
+		updateHitbox('garrett1')
+		addLuaSprite('garrett1', false)
+
+		makeLuaSprite('canYou', 'funnyAnimal/canYouBeat', 250, 450)
+		setObjectCamera('canYou', 'camother')
+		addLuaSprite('canYou', false)
+
+		makeLuaSprite('garrett2', 'funnyAnimal/obese_guy', 0, 0)
+		setProperty('garrett2.visible', false)
+		setObjectCamera('garrett2', 'camother')
+		screenCenter('garrett2', 'x')
+		screenCenter('garrett2', 'y')
+		updateHitbox('garrett2')
+		addLuaSprite('garrett2', false)
+
+		makeLuaSprite('horray', 'funnyAnimal/hooray', 250, 450)
+		setObjectCamera('horray', 'camother')
+		setProperty('horray.visible', false)
+		addLuaSprite('horray', false)
+
+		runTimer('garretChange', 3)
+		runTimer('songStart', 5)
+		return Function_Stop;
+	end
+end
+]]--
+
+function onCreate()
+	--heheheha
+	precacheImage('funnyAnimal/zunkity')
+	precacheImage('funnyAnimal/palooseCar')
+
+	if downscroll then
+		normalStrumY = screenHeight -150;
+	end
+end
+
+function onUpdate()
+	if curStep >= 3311 and curStep <= 4719 then
+		setProperty('camFollow.x', 600);
+		setProperty('camFollow.y', 350);
+	end
+end
+
+function onStepHit()
+	daCurstep = curStep;
+	if curStep == 2160 then
+		reposLegs()
+		removeLuaSprite('schoolSTATIC', true)
+		setProperty('RUNBITCH.visible', true)
+		setProperty('BFLEGS2.visible', true)
+		setProperty('defaultCamZoom', 0.75)
+		print('POLICE IS ON UR ASS, RUN!')
+	end
+
+	if curStep == 3215 then
+		setProperty('RUNBITCH.visible', false)
+		setProperty('BFLEGS2.visible', false)
+		setProperty('Jail.visible', true)
+
+		runTimer('dadMovingTimer', 1.45)
+		print('POLICE CAUGHT UR ASS!')
+	end
+
+	if curStep == 3300 then
+		setProperty('IPADBG.visible', true)
+		setProperty('IPADBG.scale.x', 0)
+		setProperty('IPADBG.scale.y', 0)
+		doTweenY('IPADBG APPEAR ON SCREEN SCALE Y', 'IPADBG.scale', 2, padZoomTime, 'quadInOut')
+		doTweenX('IPADBG APPEAR ON SCREEN SCALE X', 'IPADBG.scale', 2, padZoomTime, 'quadInOut')
+		doTweenAlpha('IPADBG APPEAR ON SCREEN', 'IPADBG', 1, padZoomTime / 2, 'quadInOut')
+
+		setProperty('IPAD.visible', true)
+		setProperty('IPAD.scale.x', 0)
+		setProperty('IPAD.scale.y', 0)
+		doTweenY('IPAD APPEAR ON SCREEN SCALE Y', 'IPAD.scale', 2, padZoomTime, 'quadInOut')
+		doTweenX('IPAD APPEAR ON SCREEN SCALE X', 'IPAD.scale', 2, padZoomTime, 'quadInOut')
+		doTweenAlpha('IPAD APPEAR ON SCREEN', 'IPAD', 1, padZoomTime / 2, 'quadInOut')
+	end
+
+	if curStep == 3310 then
+		removeLuaSprite('Jail', true)
+		setProperty('defaultCamZoom', 0.5)
+		setProperty('blackScreenBG.visible', true)
+		setProperty('Jail.visible', false)
+		setProperty('IPADBG.visible', true)
+		setProperty('IPAD.visible', true)
+
+		setProperty('strumLine.y', normalStrumY);
+		for i = 0, 4, 1 do
+			setPropertyFromGroup('opponentStrums', i, 'visible', false)
+		end
+		for i = 0, getProperty('unspawnNotes.length') -1 do
+			if getPropertyFromGroup('unspawnNotes', i, 'mustPress') == false then
+				setPropertyFromGroup('unspawnNotes', i, 'visible', false)
+			end
+		end
+		changeNoteSkin(true, 'BEVEL_NOTES')
+		local scaleXY = 0.4;
+		for i = 0, 4, 1 do
+			setPropertyFromGroup('playerStrums', i, 'scale.x', scaleXY)
+			setPropertyFromGroup('playerStrums', i, 'scale.y', scaleXY)
+
+			local offsetss = 64 * i;
+			setPropertyFromGroup('playerStrums', i, 'x', 781.5 + offsetss)
+			if downscroll then
+				setPropertyFromGroup('playerStrums', i, 'y', 535)
+			else
+				setPropertyFromGroup('playerStrums', i, 'y', 120)
+			end
+		end
+		for i = 0, getProperty('unspawnNotes.length') -1 do
+			if getPropertyFromGroup('unspawnNotes', i, 'mustPress') == true then
+				setPropertyFromGroup('unspawnNotes', i, 'scale.x', scaleXY)
+				setPropertyFromGroup('unspawnNotes', i, 'scale.y', scaleXY)
+			end
+		end
+
+		print('UH OH GARRETT IS ANGRY >:(')
+	end
+
+	if curStep == 4719 then
+		removeLuaSprite('blackScreenBG', true)
+		setProperty('defaultCamZoom', 0.8)
+		setProperty('blackScreenBG.visible', false)
+		setProperty('IPADBG.visible', false)
+		setProperty('IPAD.visible', false)
+		removeLuaSprite('IPADBG', true)
+		removeLuaSprite('IPAD', true)
+		setProperty('RUNBITCHSTATIC.visible', true)
+		for i = 0, 4, 1 do
+			setPropertyFromGroup('opponentStrums', i, 'visible', true)
+		end
+		for i = 0, getProperty('notes.length') -1 do
+			if getPropertyFromGroup('notes', i, 'mustPress') == false then --only "player" side
+				setPropertyFromGroup('notes', i, 'visible', true)
+			end
+		end
+		for i = 0, getProperty('unspawnNotes.length') -1 do
+			if getPropertyFromGroup('unspawnNotes', i, 'mustPress') == false then --only "player" side
+				setPropertyFromGroup('unspawnNotes', i, 'visible', true)
+			end
+		end
+		changeNoteSkin(true, 'NOTE_assets_3D')
+		for i = 0, getProperty('unspawnNotes.length') -1 do
+			if getPropertyFromGroup('unspawnNotes', i, 'mustPress') == true then
+				setPropertyFromGroup('unspawnNotes', i, 'scale.x', getPropertyFromGroup('playerStrums', 0, 'scale.x'))
+				setPropertyFromGroup('unspawnNotes', i, 'scale.y', getPropertyFromGroup('playerStrums', 0, 'scale.y'))
+			end
+		end
+		for i = 0, getProperty('unspawnNotes.length') -1 do
+			if getPropertyFromGroup('unspawnNotes', i, 'noteType') == 'magic note' then
+				setPropertyFromGroup('unspawnNotes', i, 'texture', 'funnyAnimal/magicNote')
+			end
+			if getPropertyFromGroup('unspawnNotes', i, 'noteType') == 'police note' then
+				setPropertyFromGroup('unspawnNotes', i, 'texture', 'funnyAnimal/palooseNote')
+			end
+		end
+		for i = 0, 4, 1 do
+			local offsetss = 115 * i;
+			setPropertyFromGroup('playerStrums', i, 'x', 750 + offsetss)
+			setPropertyFromGroup('playerStrums', i, 'y', normalStrumY)
+		end
+
+		print('magik man appeared')
+	end
+
+	if curStep == 5903 then
+		setProperty('RUNBITCHSTATIC.visible', false)
+		setProperty('RUNBITCH.visible', true)
+		print('A FUCKING PIANO?!')
+	end
+
+	if curStep == 7719 then
+		setProperty('RUNBITCHSTATIC.visible', true)
+		setProperty('RUNBITCH.visible', false)
+		doTweenX('movePiano', 'dad', -3610, 1.3)
+		print('piano leaves now')
+	end
+
+	if curStep == 8927 then
+		objectPlayAnimation('PEDOPHILESTATIC', 'hhmm')
+	end
+
+	if curStep == 8991 then
+		objectPlayAnimation('PEDOPHILESTATIC', 'smile')
+	end
+
+	if curStep == 9279 then
+		objectPlayAnimation('PEDOPHILESTATIC', 'im smile at you')
+	end
+
+	if curStep == 9347 then
+		objectPlayAnimation('PEDOPHILESTATIC', 'you ugly')
+	end
+
+	if curStep == 9420 then
+		objectPlayAnimation('PEDOPHILESTATIC', 'did you get uglier')
+	end
+
+	if curStep == 9503 then
+		objectPlayAnimation('PEDOPHILESTATIC', 'garrett is ugly')
+	end
+
+	if curStep == 9759 then
+		objectPlayAnimation('PEDOPHILESTATIC', 'bf is ugly')
+	end
+
+	if curStep == 10015 then
+		objectPlayAnimation('PEDOPHILESTATIC', 'like my cut')
+	end
+
+	if curStep == 10271 then
+		objectPlayAnimation('PEDOPHILESTATIC', 'i wear a mask with a smile')
+	end
+
+	if curStep == 10527 then
+		objectPlayAnimation('PEDOPHILESTATIC', 'wtf')
+	end
+
+	if curStep == 10863 then
+		objectPlayAnimation('PEDOPHILESTATIC', 'THERE IS A CAR COMING RUN BITCH')
+	end
+
+	if curStep == 11038 then
+		setProperty('PEDOPHILESTATIC.visible', false)
+		removeLuaSprite('PEDOPHILESTATIC', true)
+		setProperty('blackScreen.visible', true)
+		setProperty('RUNBITCHSTATIC.visible', false)
+		removeLuaSprite('RUNBITCHSTATIC', true)
+		setProperty('RUNBITCH.visible', true)
+
+		makeAnimatedLuaSprite('POLICECAR', 'funnyAnimal/palooseCar', getProperty('dad.x'), getProperty('dad.y'))
+		addAnimationByPrefix('POLICECAR', 'run', 'idle0', 24, true)
+		objectPlayAnimation('POLICECAR', 'run', true)
+		setProperty('POLICECAR.antialiasing', getPropertyFromClass('ClientPrefs', 'globalAntialiasing'))
+		addLuaSprite('POLICECAR', true)
+		print('CAR IS HERE LOL')
+	end
+
+	if curStep == 11043 then
+		setProperty('blackScreen.visible', false)
+		removeLuaSprite('blackScreen', true)
+		setProperty('BFLEGS2.visible', true)
+		setProperty('BFLEGS2.flipX', false)
+		reposLegs()
+	end
+
+	if curStep == 11295 then
+		setProperty('defaultCamZoom', 1.2)
+	end
+
+	if curStep == 11423 then
+		setProperty('defaultCamZoom', 0.8)
+	end
+end
+
+--[[
+function onEndSong()
+	if not allowSongEnd then --Block the first countdown
+		makeLuaSprite('VICTORY', 'funnyAnimal/end', 0, 0)
+		setObjectCamera('VICTORY', 'camhud')
+		setProperty('VICTORY.width', 1280)
+		setProperty('VICTORY.height', 720)
+		screenCenter('VICTORY', 'x')
+		screenCenter('VICTORY', 'y')
+		updateHitbox('VICTORY')
+		addLuaSprite('VICTORY', false)
+		playSound('win')
+		runTimer('songEnd', 4)
+		return Function_Stop;
+	end
+
+	return Function_Continue;
+end
+-]]
+
+function onEvent(n, v1, v2)
+	if n == 'Change Character' then
+		if v2 == 'garrett-car' or v2 == 'garrett-angry' or v2 == 'pedophile' or v2 == '3d-bf' or v2 == '3d-bf-flipped' then			
+			if v2 == 'garrett-angry' then
+				setProperty('PEDOPHILESTATIC.x', 1950)
+				setProperty('PEDOPHILESTATIC.y', 675)
+				setProperty('PEDOPHILESTATIC.visible', true)
+				objectPlayAnimation('PEDOPHILESTATIC', 'hey its the toddler')
+				print('pedophile 2 is now shown')
+			end
+
+			if v2 == 'pedophile' then
+				makeAnimatedLuaSprite('PEDOPHILESTATIC', 'funnyAnimal/zunkity', getProperty('dad.x') -300, getProperty('dad.y'))
+				addAnimationByPrefix('PEDOPHILESTATIC', 'hey its the toddler', 'FAKE LOADING SCREEN0000', 24, false)
+				addAnimationByPrefix('PEDOPHILESTATIC', 'hhmm', 'FAKE LOADING SCREEN0001', 24, false)
+				addAnimationByPrefix('PEDOPHILESTATIC', 'smile', 'FAKE LOADING SCREEN0002', 24, false)
+				addAnimationByPrefix('PEDOPHILESTATIC', 'im smile at you', 'FAKE LOADING SCREEN0003', 24, false)
+				addAnimationByPrefix('PEDOPHILESTATIC', 'you ugly', 'FAKE LOADING SCREEN0004', 24, false)
+				addAnimationByPrefix('PEDOPHILESTATIC', 'did you get uglier', 'FAKE LOADING SCREEN0005', 24, false)
+				addAnimationByPrefix('PEDOPHILESTATIC', 'garrett is ugly', 'FAKE LOADING SCREEN0006', 24, false)
+				addAnimationByPrefix('PEDOPHILESTATIC', 'bf is ugly', 'FAKE LOADING SCREEN0007', 24, false)
+				addAnimationByPrefix('PEDOPHILESTATIC', 'like my cut', 'FAKE LOADING SCREEN0008', 24, false)
+				addAnimationByPrefix('PEDOPHILESTATIC', 'i wear a mask with a smile', 'FAKE LOADING SCREEN0009', 24, false)
+				addAnimationByPrefix('PEDOPHILESTATIC', 'wtf', 'FAKE LOADING SCREEN0010', 24, false)
+				addAnimationByPrefix('PEDOPHILESTATIC', 'THERE IS A CAR COMING RUN BITCH', 'FAKE LOADING SCREEN0011', 24, false)
+				setProperty('PEDOPHILESTATIC.antialiasing', getPropertyFromClass('ClientPrefs', 'globalAntialiasing'))
+				setProperty('PEDOPHILESTATIC.visible', false)
+				addLuaSprite('PEDOPHILESTATIC', false)
+				print('pedophile sprite made')
+			end
+		else
+			cameraFlash('other', 'FFFFFF', 0.35)
+		end
+	end
+end
+
+function onTimerCompleted(tag, loops, loopsLeft)
+	if tag == 'garretChange' then
+		setProperty('garrett1.visible', false)
+		setProperty('canYou.visible', false)
+		setProperty('garrett2.visible', true)
+		setProperty('horray.visible', true)
+		playSound('win')
+	end
+
+	if tag == 'songStart' then
+		setProperty('greenScreen.visible', false)
+		setProperty('garrett1.visible', false)
+		setProperty('canYou.visible', false)
+		setProperty('garrett2.visible', false)
+		setProperty('canYou.visible', false)
+		setProperty('horray.visible', false)
+		allowCountdown = true;
+	end
+
+	if tag == 'songEnd' then
+		allowSongEnd = true;
+	end
+
+	if tag == 'dadMovingTimer' then
+		doTweenX('moveDad', 'dad', 4640, 6.5)
+	end
+end
+
+function onTweenCompleted(tag)
+	if tag == 'movePiano' then
+        triggerEvent('Change Character', 'dad', 'pedophile')
+	end
+end
+
+function reposLegs()
+	setProperty('BFLEGS2.x', getProperty("boyfriend.x") - 130)
+	setProperty('BFLEGS2.y', getProperty("boyfriend.y") + 200)
+
+	if getProperty("BFLEGS2.flipX") == false then
+		setProperty('BFLEGS2.x', getProperty("BFLEGS2.x") + 100)
+	end
+end
+
+function changeNoteSkin(player, skin)
+	if player == true then
+		for i = 0, 4, 1 do
+			setPropertyFromGroup('playerStrums', i, 'texture', skin)
+		end
+	end
+    if not player then
+		for i = 0, 4, 1 do
+			setPropertyFromGroup('opponentStrums', i, 'texture', skin)
+		end
+	end
+
+    for i = 0, getProperty('notes.length') -1 do
+        if getPropertyFromGroup('notes', i, 'mustPress') == player then --only "player" side
+            setPropertyFromGroup('notes', i, 'texture', skin)
+        end
+    end
+
+    for i = 0, getProperty('unspawnNotes.length') -1 do
+        if getPropertyFromGroup('unspawnNotes', i, 'mustPress') == player then --only "player" side
+            setPropertyFromGroup('unspawnNotes', i, 'texture', skin)
+        end
+    end
+end
